15b1f8b672d93d31fd4002163e0f30ba19b9f24c
onlyhall <dm.onlyhal@gmail.com>
2014-12-10 22:33:48 +0200

    Создал модель для таблицы User. Создал уебанскую функцию view в ArticleController и представление для нее. Через неё можно достать запись любой таблицы.

--- app/Controller/ArticleController.php
+++ app/Controller/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
     class ArticleController extends AppController{
         public $helper = array('Html', 'Form');
+        public $uses = array('District', 'City', 'University', 'User', 'Article');
         public function index($id){
             $this->set('article', $this->Article->find('all', $params = array(
                                                         'conditions' => array('Article.id_university' => $id),
@@ -8,7 +9,15 @@
             $this->set('title', 'Вибір статей');
         }
         public function view($id){
-            $this->set('article', $this->Article->findById($id));
-            $this->set('title', 'стаття');
+            $query ="SELECT * FROM Articles INNER JOIN Users on Articles.id_user = Users.id "
+                    . "                     INNER JOIN Universities ON Articles.id_university = Universities.id"
+                    . "                     INNER JOIN Cities on Universities.id_city = Cities.id"
+                    . "                     INNER JOIN Districts on Cities.id_district = Districts.id WHERE Articles.id = $id";
+            $article = $this->Article->query($query);
+            $this->set('article', $article);
+            
+            foreach ($article as $article):
+            $this->set('title', $article['Articles']['caption']);
+            endforeach;
         }
     }
--- app/View/Article/view.ctp
+++ app/View/Article/view.ctp
@@ -1,11 +1,23 @@
 <?php 
     foreach ($article as $article): ?>
 <h1>
-    <?php    echo $article['caption']; ?>
+    <?php    echo $article['Articles']['caption']; ?>
 </h1>
     <?php
-        echo $article['text'];
-?>
+        echo $article['Articles']['text'];
+    ?>
+<h2>
+    <?php echo $article['Users']['login']; ?>
+</h2>
+<h2>
+    <?php echo $article['Universities']['name']; ?>
+</h2>
+<h2>
+    <?php echo $article['Cities']['name']; ?>
+</h2>
+<h2>
+    <?php echo $article['Districts']['name']; ?>
+</h2>
     <?php endforeach; ?>
     
     <?php unset($article);
